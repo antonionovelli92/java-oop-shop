87e6e9d6715a902b6a6c7f30695527007f93a5fd
antonionovelli92 <anto.novelli@hotmail.it>
2023-05-08 17:18:12 +0200

    2' ex

--- src/jana60/bank/Conto.java
+++ src/jana60/bank/Conto.java
@@ -1,5 +1,57 @@
 package jana60.bank;
 
-public class Conto {
+import java.util.Random;
 
+public class Conto {
+    private final int numeroConto;
+    private String nomeProprietario;
+    private double saldo;
+
+    public Conto(String nomeProprietario) {
+        this.numeroConto = new Random().nextInt(1000) + 1;
+        this.nomeProprietario = nomeProprietario;
+        this.saldo = 0;
+    }
+
+    public int getNumeroConto() {
+        return this.numeroConto;
+    }
+
+    public String getNomeProprietario() {
+        return this.nomeProprietario;
+    }
+
+    public void setNomeProprietario(String nomeProprietario) {
+        this.nomeProprietario = nomeProprietario;
+    }
+
+    public double getSaldo() {
+        return this.saldo;
+    }
+
+    public boolean versa(double importo) {
+        if (importo > 0) {
+            this.saldo += importo;
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    public boolean preleva(double importo) {
+        if (importo > 0 && this.saldo - importo >= 0) {
+            this.saldo -= importo;
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    public String getInfoConto() {
+        return "Numero conto: " + this.numeroConto + ", Proprietario: " + this.nomeProprietario;
+    }
+
+    public String getSaldoFormattato() {
+        return String.format("%.2f", this.saldo) + "€";
+    }
 }
--- src/jana60/bank/Main.java
+++ src/jana60/bank/Main.java
@@ -1,5 +1,54 @@
 package jana60.bank;
 
-public class Main {
+import java.util.Scanner;
 
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Inserisci il tuo nome: ");
+        String nome = scanner.nextLine();
+
+        Conto conto = new Conto(nome);
+
+        while (true) {
+            System.out.println("1. Versa una somma");
+            System.out.println("2. Preleva una somma");
+            System.out.println("3. Exit");
+
+            System.out.print("Cosa vuoi fare? ");
+            int scelta = scanner.nextInt();
+
+            if (scelta == 1) {
+                System.out.print("Quanto vuoi versare? ");
+                double importo = scanner.nextDouble();
+
+                if (conto.versa(importo)) {
+                    System.out.println("Operazione effettuata con successo.");
+                } else {
+                    System.out.println("Operazione non valida.");
+                }
+
+                System.out.println("Saldo attuale: " + conto.getSaldoFormattato());
+            } else if (scelta == 2) {
+                System.out.print("Quanto vuoi prelevare? ");
+                double importo = scanner.nextDouble();
+
+                if (conto.preleva(importo)) {
+                    System.out.println("Operazione effettuata con successo.");
+                } else {
+                    System.out.println("Operazione non valida.");
+                }
+
+                System.out.println("Saldo attuale: " + conto.getSaldoFormattato());
+            } else if (scelta == 3) {
+            	System.out.println("Arriverderci e scappa.");
+                break;
+            } else {
+                System.out.println("Scelta non valida.");
+            }
+        }
+
+        scanner.close();
+    }
 }
